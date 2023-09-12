eebd459b7d58dac4488a1c68f990345f102ce798
RDJ20 <rafael.sandovalg4@gmail.com>
2023-09-11 23:01:51 -0600

    agregado server,py

--- ProyectoTostadora/NodeJs/index.js
+++ ProyectoTostadora/NodeJs/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const routes = require('./routes/routes'); 
+const routes = require('./routes/routes');
+const cors = require('cors');
+const { exec } = require('child_process'); // Importar el módulo child_process
+
 const app = express();
 
 app.use(express.json());
-
-const cors = require('cors');
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -12,12 +13,19 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api', routes); 
-
+app.use('/api', routes);
 app.use(express.static('../Pagina Web/Login'));
 
-
-app.listen(3000,  '0.0.0.0',() => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Servidor iniciado en el puerto 3000');
 
+  // Ejecutar el script de Python
+  exec('python3 tu_archivo.py', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error al ejecutar el archivo Python: ${error}`);
+      return;
+    }
+    console.log(`Salida del script Python: ${stdout}`);
+    console.error(`Errores del script Python: ${stderr}`);
+  });
 });
