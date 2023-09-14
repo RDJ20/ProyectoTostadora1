26a4afdc35a28bad61ad5b44dca3d0902cbee6c4
RDJ20 <rafael.sandovalg4@gmail.com>
2023-09-13 21:28:52 -0600

    Funcion 3

--- ProyectoTostadora/Pagina Web/Login/admin/grafica.js
+++ ProyectoTostadora/Pagina Web/Login/admin/grafica.js
@@ -281,3 +281,38 @@ actualizarTiempo(tomarDatos(), tomarDatos2());
 }
 
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  var token = localStorage.getItem('jwtToken');
+
+  fetch(`${baseUrl}/api/admin`, {
+    method: 'GET', 
+    headers: {
+      'Authorization': 'Bearer ' + token,
+    }
+  })
+  .then(response => {
+    if(response.status === 403) {
+      window.location.href = 'http://raspberrypi.local:3000/index.html';
+    }
+    return response.json();
+  })
+  .then(data => {
+    document.getElementById('mensaje').textContent = '¡Hola Admin!';
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+  function cerrarSesion() {
+    localStorage.removeItem('jwtToken');
+    window.location.href = 'http://raspberrypi.local:3000/index.html';
+    console.log("Funciona el boton de cerrar sesion");
+}
+
+  document.getElementById('BotonCerrarSesion').addEventListener('click', cerrarSesion);
+  document.getElementById("Guardar").addEventListener("click", enviarPerfil   );
+  document.getElementById("iniciar").addEventListener('click', comenzarGrafica);
+  document.getElementById("setear") .addEventListener('click', setear         );
+  
+
+});
